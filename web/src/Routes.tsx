bd4597613b966295f0ffbbbaa2b9cabfae64eb75
patled <github@lederer.cloud>
2024-05-10 17:35:47 +0200

    Generate dbauth pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -16,6 +16,10 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={ScaffoldLayout} title="Tasks" titleTo="tasks" buttonLabel="New Task" buttonTo="newTask">
         <Route path="/tasks/new" page={TaskNewTaskPage} name="newTask" />
         <Route path="/tasks/{id}/edit" page={TaskEditTaskPage} name="editTask" />
