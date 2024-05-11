5124029b3a7c047780c63c9a3aac24ab763b7480
patled <github@lederer.cloud>
2024-05-11 08:13:06 +0200

    refactor: Improve TaskList component structure and styling

--- web/src/components/TaskList/TaskList.tsx
+++ web/src/components/TaskList/TaskList.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react'
 
 import type { FindTasks, Task } from 'types/graphql'
 
+import { Link, routes } from '@redwoodjs/router'
+
 import { SpeechServiceContext } from 'src/services/SpeechServiceContext'
 
 import './TaskList.css'
@@ -37,12 +39,26 @@ const TaskList = ({ tasks }: FindTasks) => {
 
       <div className="task-list">
         {tasks.map((task) => (
-          <div key={task.id} className="task" onClick={() => speak(task)}>
-            {task.imageUrl && <img src={task.imageUrl} alt="image" />}
-            {task.icon && !task.imageUrl && (
-              <span className="material-icons-outlined">{task.icon}</span>
-            )}
-            <p>{task.audioText}</p>
+          <div key={task.id} className="task">
+            <div className="actions">
+              <Link
+                to={routes.editTask({ id: task.id })}
+                title={'Edit task ' + task.id}
+                className="rw-button rw-button-small rw-button-blue"
+              >
+                Edit
+              </Link>
+            </div>
+
+            <div className="task-content" onClick={() => speak(task)}>
+              {task.imageUrl && <img src={task.imageUrl} alt="image" />}
+              {task.icon && !task.imageUrl && (
+                <span className="material-icons-outlined icon">
+                  {task.icon}
+                </span>
+              )}
+              <p>{task.audioText}</p>
+            </div>
           </div>
         ))}
       </div>
