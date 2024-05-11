7b01170efaee39bffd108753a25d32a814ace34c
patled <github@lederer.cloud>
2024-05-11 07:36:54 +0200

    feat: Update Routes.tsx to include new task-related routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -18,17 +18,17 @@ const Routes = () => {
     <Router useAuth={useAuth}>
       <PrivateSet unauthenticated="login">
         <Route path="/home" page={HomePage} name="home" />
+        <Set wrap={ScaffoldLayout} title="Tasks" titleTo="tasks" buttonLabel="New Task" buttonTo="newTask">
+          <Route path="/tasks/new" page={TaskNewTaskPage} name="newTask" />
+          <Route path="/tasks/{id}/edit" page={TaskEditTaskPage} name="editTask" />
+          <Route path="/tasks/{id}" page={TaskTaskPage} name="task" />
+          <Route path="/tasks" page={TaskTasksPage} name="tasks" />
+        </Set>
       </PrivateSet>
       <Route path="/login" page={LoginPage} name="login" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Set wrap={ScaffoldLayout} title="Tasks" titleTo="tasks" buttonLabel="New Task" buttonTo="newTask">
-        <Route path="/tasks/new" page={TaskNewTaskPage} name="newTask" />
-        <Route path="/tasks/{id}/edit" page={TaskEditTaskPage} name="editTask" />
-        <Route path="/tasks/{id}" page={TaskTaskPage} name="task" />
-        <Route path="/tasks" page={TaskTasksPage} name="tasks" />
-      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
