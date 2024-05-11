b8da41574bc508c4d3c5fbf7c30d012b6c3c85d3
patled <github@lederer.cloud>
2024-05-11 07:05:05 +0200

    feat: Add IconHint component for displaying hints with icons

---
+++ web/src/components/IconHint/IconHint.tsx
@@ -0,0 +1,16 @@
+import './IconHint.css'
+export function IconHint() {
+  return (
+    <span>
+      Open{' '}
+      <a
+        href="https://jossef.github.io/material-design-icons-iconfont/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Iconpage
+      </a>{' '}
+      and copy the name of the icon you want to use.
+    </span>
+  )
+}
